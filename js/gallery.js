dbb93a1dbdfb397e13131a7cae562b745b6cf759
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2022-07-27 19:53:52 +0200

    Add simpe function for creating elems from json

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,16 @@
 import { Fancybox } from "https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.esm.js";
+import inputJson from "./gallery.json" assert { type: "json" }
+
+function createGalleryItem() {
+  const gallery = document.querySelector('.carousel')
+
+  for (let i = 0; i < Object.keys(inputJson).length; i++) {
+    gallery.innerHTML += `<a data-fancybox="demo" href="${inputJson[i].image_url}">
+    ${inputJson[i].title} <img class="rounded" src="${inputJson[i].image_url}" /></a>`
+  }
+}
+
+createGalleryItem()
 
 Fancybox.bind('[data-fancybox="gallery"]', {
     dragToClose: false,
